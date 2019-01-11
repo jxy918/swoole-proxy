99d3623b7a0fd5fd03f596ff8cd54258860cac20
jxy918 <jxy918@163.com>
2019-01-11 11:23:22 +0800

    优化

--- ProxyServer.php
+++ ProxyServer.php
@@ -42,16 +42,16 @@ class ProxyServer {
      * 服务器配置,这里设置很关键， 要了解c++服务器的包头+包体
      */         
     protected $serv_conf = array(
-		'dispatch_mode' => 2,	//注意，无状态模式，可以选择2,4,5，具体解释请参考swoole文档		
-		'open_length_check'     => true,
-		'package_length_type'   => 'N',
-		'package_length_offset' => 0,          		
-		'package_body_offset'   => 0,  		
-		'package_max_length'    => 1024 * 1024,
-		'socket_buffer_size'    => 1024 * 1024,
-        
+        'dispatch_mode' => 2,	//注意，无状态模式，可以选择2,4,5，具体解释请参考swoole文档
+        'open_length_check'     => true,
+        'package_length_type'   => 'N',
+        'package_length_offset' => 0,
+        'package_body_offset'   => 0,
+        'package_max_length'    => 1024 * 1024,
+        'socket_buffer_size'    => 1024 * 1024,
+
         'worker_num' => 2, //设置启动的worker进程数。
-        'max_conn' => 10000, //服务器程序，最大允许的连接数 
+        'max_conn' => 10000, //服务器程序，最大允许的连接数
         'backlog' => 128, //Listen队列长度，如backlog => 128，此参数将决定最多同时有多少个等待accept的连接
         'daemonize'=>0,  //守护进程化。设置daemonize => 1时，程序将转入后台作为守护进程运行
         'log_file' => './ProxyServer.log', //swoole error log
@@ -62,39 +62,39 @@ class ProxyServer {
      * 运行服务器
      */         
     public function run() {
-        $this->serv = new Swoole\Websocket\Server($this->serv_ip, $this->serv_port);				
-		$this->serv->set($this->serv_conf);		
-		$this->serv->on('Start', array($this, 'onStart'));
-		$this->serv->on('ManagerStart', array($this, 'onManagerStart'));
-		$this->serv->on('WorkerStart', array($this, 'onWorkerStart'));
-		$this->serv->on('Open', array($this, 'onOpen'));
-		$this->serv->on('Message', array($this, 'onMessage'));
-		$this->serv->on('Close', array($this, 'onClose'));
-		$this->serv->start();
+        $this->serv = new Swoole\Websocket\Server($this->serv_ip, $this->serv_port);
+        $this->serv->set($this->serv_conf);
+        $this->serv->on('Start', array($this, 'onStart'));
+        $this->serv->on('ManagerStart', array($this, 'onManagerStart'));
+        $this->serv->on('WorkerStart', array($this, 'onWorkerStart'));
+        $this->serv->on('Open', array($this, 'onOpen'));
+        $this->serv->on('Message', array($this, 'onMessage'));
+        $this->serv->on('Close', array($this, 'onClose'));
+        $this->serv->start();
     }
 
     public function onStart($serv) {
-		swoole_set_process_name(self::PROCESS_NAME_PREFIX.": master: ".get_called_class());
+        swoole_set_process_name(self::PROCESS_NAME_PREFIX.": master: ".get_called_class());
         $this->log("************************************************************************************");
         $this->log("* PROXYWS  |  HOST: \e[0;32m{$this->serv_ip}\e[0m, PORT:\e[0;32m{$this->serv_port}\e[0m, MODE:\e[0;32m{$this->serv_conf['dispatch_mode']}\e[0m, WORKER:\e[0;32m{$this->serv_conf['worker_num']}\e[0m");
-		$this->log("* BACKTCP  |  HOST: \e[0;32m{$this->back_serv['ip']}\e[0m, PORT:\e[0;32m{$this->back_serv['port']}\e[0m");
+        $this->log("* BACKTCP  |  HOST: \e[0;32m{$this->back_serv['ip']}\e[0m, PORT:\e[0;32m{$this->back_serv['port']}\e[0m");
         $this->log("* MasterPid={$serv->master_pid}  |  ManagerPid={$serv->manager_pid}  |  Swoole version is [" . SWOOLE_VERSION . "]");
         $this->log("************************************************************************************");
 	}
 
-	public function onManagerStart($serv) {
-		swoole_set_process_name(self::PROCESS_NAME_PREFIX.": manager: ".get_called_class());
-		$this->log("onManagerStart:");	
-	}
+    public function onManagerStart($serv) {
+        swoole_set_process_name(self::PROCESS_NAME_PREFIX.": manager: ".get_called_class());
+        $this->log("onManagerStart:");
+    }
 
-	public function onWorkerStart($serv, $worker_id) {
-		swoole_set_process_name(self::PROCESS_NAME_PREFIX.": worker {$worker_id}: ".get_called_class());
-		$this->log("onWorkerStart:");	
-	}
+    public function onWorkerStart($serv, $worker_id) {
+        swoole_set_process_name(self::PROCESS_NAME_PREFIX.": worker {$worker_id}: ".get_called_class());
+        $this->log("onWorkerStart:");
+    }
 
     /**
      * 处理websocket关闭回调
-     */         
+     */
     public function onClose($serv, $fd) {
         //处理关闭链接，  网关服务器=>关闭异步客户端=》通知到代理服务器=》代理服务器关闭websocket前端
         if (isset($this->clients[$fd])) {
@@ -103,7 +103,7 @@ class ProxyServer {
             if($backend_client->isConnected()) {
                 $backend_client->close();
             }
-            
+
             unset($this->backends[$backend_client->sock]);
             $this->log("client close");
         }
@@ -111,7 +111,7 @@ class ProxyServer {
 
     /**
      * 处理websocket连接成功后回调
-     */         
+     */
     public function onOpen($server, $frame) {
         //启动异步客户端
         $socket = new swoole_client(SWOOLE_SOCK_TCP, SWOOLE_SOCK_ASYNC);
@@ -123,32 +123,31 @@ class ProxyServer {
             $this->clients[$frame->fd] = array(
                 'socket' => $socket,
             );
-        
             $this->log("connect to backend server success");
             $this->log(": Client[$frame->fd] backend-sock[{$socket->sock}]: Connect.");
         });
         $socket->on('error', function ($socket) {
             $this->log("connect to backend server fail");
         });
-		$socket->on('close', function ($socket) {
+        $socket->on('close', function ($socket) {
             //处理服务器断线
             $fd = isset($this->backends[$socket->sock]['client_fd']) ? $this->backends[$socket->sock]['client_fd'] : NULL ;
-            if(!empty($fd) && $this->serv->connection_info($fd)) {                              
+            if(!empty($fd) && $this->serv->connection_info($fd)) {
                 $this->serv->close($fd);
                 $this->log("server close websocket");
             }
         });
-        $socket->on('receive', function ($socket, $data){			
+        $socket->on('receive', function ($socket, $data){
             $fd = isset($this->backends[$socket->sock]['client_fd']) ? $this->backends[$socket->sock]['client_fd'] : NULL ;
             if(!empty($fd) && $this->serv->connection_info($fd)) {
-                $this->log('websocket: Recv <<<<< :  client_fd='.$fd.'  len='.strlen($data)); 
-    			$this->serv->push($fd, $data, WEBSOCKET_OPCODE_BINARY);
+                $this->log('websocket: Recv <<<<< :  client_fd='.$fd.'  len='.strlen($data));
+                $this->serv->push($fd, $data, WEBSOCKET_OPCODE_BINARY);
             }
         });
-		
+
         $socket->connect($this->back_serv['ip'], $this->back_serv['port'], $this->back_serv['timeout']);
-		//统计连接信息
-		$stats = $server->stats();
+        //统计连接信息
+        $stats = $server->stats();
         $this->log("connection_num:{$stats['connection_num']}，accept_count:{$stats['accept_count']}，close_count:{$stats['close_count']}");
     }
 
--- ProxyServerByCo.php
+++ ProxyServerByCo.php
@@ -42,16 +42,16 @@ class ProxyServerByCo {
      * 服务器配置,这里设置很关键， 要了解c++服务器的包头+包体
      */         
     protected $serv_conf = array(
-		'dispatch_mode' => 3,
-		'open_length_check'     => true,
-		'package_length_type'   => 'N',
-		'package_length_offset' => 0,          		
-		'package_body_offset'   => 0,  		
-		'package_max_length'    => 1024 * 1024,
-		'socket_buffer_size'    => 1024 * 1024,
-        
+        'dispatch_mode' => 3,
+        'open_length_check'     => true,
+        'package_length_type'   => 'N',
+        'package_length_offset' => 0,
+        'package_body_offset'   => 0,
+        'package_max_length'    => 1024 * 1024,
+        'socket_buffer_size'    => 1024 * 1024,
+
         'worker_num' => 2, //设置启动的worker进程数。
-        'max_conn' => 10000, //服务器程序，最大允许的连接数 
+        'max_conn' => 10000, //服务器程序，最大允许的连接数
         'backlog' => 128, //Listen队列长度，如backlog => 128，此参数将决定最多同时有多少个等待accept的连接
         'daemonize'=>0,  //守护进程化。设置daemonize => 1时，程序将转入后台作为守护进程运行
         'log_file' => './ProxyServer.log', //swoole error log
@@ -60,36 +60,37 @@ class ProxyServerByCo {
 
     /**
      * 运行服务器
-     */         
+     */
     public function run() {
-        $this->serv = new Swoole\Websocket\Server($this->serv_ip, $this->serv_port);				
-		$this->serv->set($this->serv_conf);		
-		$this->serv->on('Start', array($this, 'onStart'));
-		$this->serv->on('ManagerStart', array($this, 'onManagerStart'));
-		$this->serv->on('WorkerStart', array($this, 'onWorkerStart'));
-		$this->serv->on('Open', array($this, 'onOpen'));
-		$this->serv->on('Message', array($this, 'onMessage'));
-		$this->serv->on('Close', array($this, 'onClose'));
-		$this->serv->start();
+        $this->serv = new Swoole\Websocket\Server($this->serv_ip, $this->serv_port);
+        $this->serv->set($this->serv_conf);
+        $this->serv->on('Start', array($this, 'onStart'));
+        $this->serv->on('ManagerStart', array($this, 'onManagerStart'));
+        $this->serv->on('WorkerStart', array($this, 'onWorkerStart'));
+        $this->serv->on('Open', array($this, 'onOpen'));
+        $this->serv->on('Message', array($this, 'onMessage'));
+        $this->serv->on('Close', array($this, 'onClose'));
+        $this->serv->start();
     }
 
     public function onStart($serv) {
-		swoole_set_process_name(self::PROCESS_NAME_PREFIX.": master: ".get_called_class());
-		$this->log("MasterPid={$serv->master_pid}");
-        $this->log("ManagerPid={$serv->manager_pid}");
-        $this->log("Server: start.Swoole version is [" . SWOOLE_VERSION . "]");
-        $this->log("IP: \e[0;32m{$this->serv_ip}\e[0m, PORT:\e[0;32m{$this->serv_port}\e[0m PROXY_IP:\e[0;32m{$this->back_serv['ip']}\e[0m, PROXY_PORT:\e[0;32m{$this->back_serv['port']}\e[0m, PROXY_TIMEOUT:\e[0;32m{$this->back_serv['timeout']}\e[0m");
+        swoole_set_process_name(self::PROCESS_NAME_PREFIX.": master: ".get_called_class());
+        $this->log("************************************************************************************");
+        $this->log("* PROXYWS  |  HOST: \e[0;32m{$this->serv_ip}\e[0m, PORT:\e[0;32m{$this->serv_port}\e[0m, MODE:\e[0;32m{$this->serv_conf['dispatch_mode']}\e[0m, WORKER:\e[0;32m{$this->serv_conf['worker_num']}\e[0m");
+        $this->log("* BACKTCP  |  HOST: \e[0;32m{$this->back_serv['ip']}\e[0m, PORT:\e[0;32m{$this->back_serv['port']}\e[0m");
+        $this->log("* MasterPid={$serv->master_pid}  |  ManagerPid={$serv->manager_pid}  |  Swoole version is [" . SWOOLE_VERSION . "]");
+        $this->log("************************************************************************************");
     }
 
-	public function onManagerStart($serv) {
-		swoole_set_process_name(self::PROCESS_NAME_PREFIX.": manager: ".get_called_class());
-		$this->log("onManagerStart:");	
-	}
+    public function onManagerStart($serv) {
+        swoole_set_process_name(self::PROCESS_NAME_PREFIX.": manager: ".get_called_class());
+        $this->log("onManagerStart:");
+    }
 
-	public function onWorkerStart($serv, $worker_id) {
-		swoole_set_process_name(self::PROCESS_NAME_PREFIX.": worker {$worker_id}: ".get_called_class());
-		$this->log("onWorkerStart:");	
-	}
+    public function onWorkerStart($serv, $worker_id) {
+        swoole_set_process_name(self::PROCESS_NAME_PREFIX.": worker {$worker_id}: ".get_called_class());
+        $this->log("onWorkerStart:");
+    }
 
     /**
      * 处理websocket关闭回调
@@ -99,10 +100,10 @@ class ProxyServerByCo {
         if (isset($this->clients[$fd])) {
             $backend_client = $this->clients[$fd]['socket'];
             unset($this->clients[$fd]);
-			if($backend_client->isConnected()) {
+            if($backend_client->isConnected()) {
                 $backend_client->close();
             }
-			$backend_client->close();           
+            $backend_client->close();
             unset($this->backends[$backend_client->sock]);
             $this->log("client close");
         }
@@ -110,7 +111,7 @@ class ProxyServerByCo {
 
     /**
      * 处理websocket连接成功后回调
-     */         
+     */
     public function onOpen($server, $frame) {
         //启用协程客户端
         $client = new Swoole\Coroutine\Client(SWOOLE_SOCK_TCP | SWOOLE_KEEP);
@@ -130,49 +131,52 @@ class ProxyServerByCo {
         );
         $this->log("connect to backend server success");
         $this->log("Client[$frame->fd] backend-sock[{$client->sock}]: Connect.");
+        //统计连接信息
+        $stats = $server->stats();
+        $this->log("connection_num:{$stats['connection_num']}，accept_count:{$stats['accept_count']}，close_count:{$stats['close_count']}");
     }
 
     /**
      * 处理接受消息回调
-     */         
+     */
     public function onMessage($server, $frame) {
         if(!empty($frame)) {
             $begin_time =  $this->_getMicTime();
             $backend_socket = isset($this->clients[$frame->fd]['socket']) ? $this->clients[$frame->fd]['socket'] : NULL;
             if(!empty($backend_socket) && $backend_socket->isConnected()) {
-				//发送数据
+                //发送数据
                 $ret = $backend_socket->send($frame->data);
                 $end_time = $this->_getMicTime();
                 $this->log('websocket: Send >>>>> :  client_fd='.$frame->fd.'  len='.$ret.'  speed_time='.($end_time-$begin_time));
-				//接受数据
+                //接受数据
                 try {
                     $data = $backend_socket->recv();
                 } catch (Exception $e) {
                     $this->log($backend_socket->errCode());
                     $this->log($e->getMessage());
                 }
-				$fd = isset($this->backends[$backend_socket->sock]['client_fd']) ? $this->backends[$backend_socket->sock]['client_fd'] : NULL ;
-				if(!empty($fd) && $this->serv->connection_info($fd) && !empty($data)) {
-					$this->log('websocket: Recv <<<<< :  client_fd='.$fd.'  len='.strlen($data)); 
-					$this->serv->push($fd, $data, WEBSOCKET_OPCODE_BINARY);
-				}				
+                $fd = isset($this->backends[$backend_socket->sock]['client_fd']) ? $this->backends[$backend_socket->sock]['client_fd'] : NULL ;
+                if(!empty($fd) && $this->serv->connection_info($fd) && !empty($data)) {
+                    $this->log('websocket: Recv <<<<< :  client_fd='.$fd.'  len='.strlen($data));
+                    $this->serv->push($fd, $data, WEBSOCKET_OPCODE_BINARY);
+                }
             } else {
-				$this->log("connect to backend server fail");
-				//清理socket
-				if(!empty($backend_socket)) {
-					$fd = isset($this->backends[$backend_socket->sock]['client_fd']) ? $this->backends[$backend_socket->sock]['client_fd'] : NULL ;
-					if(!empty($fd) && $this->serv->connection_info($fd)) {                              
-						$this->serv->close($fd);
-						$this->log("server close websocket");
-					}
-				}
-			}	
+                $this->log("connect to backend server fail");
+                //清理socket
+                if(!empty($backend_socket)) {
+                    $fd = isset($this->backends[$backend_socket->sock]['client_fd']) ? $this->backends[$backend_socket->sock]['client_fd'] : NULL ;
+                    if(!empty($fd) && $this->serv->connection_info($fd)) {
+                        $this->serv->close($fd);
+                        $this->log("server close websocket");
+                    }
+                }
+            }
         }
     }
     
     /**
      * 打印输出
-     */         
+     */
     public function log($content, $level = 'DEBUG') {
         $content = '['.date('Y-m-d H:i:s)').']   '.$level.'   '.$content."\n";
         echo $content;
